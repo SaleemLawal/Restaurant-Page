58cd530f8696694f16d6f7488dcc6ff7167a3b55
SaleemLawal <slawal1@umbc.edu>
2024-01-11 09:17:41 -0500

    Updated Webpack to commonJS syntax, updated file directory to contain parent folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-export const mode = 'development';
-export const devtool = 'inline-source-map';
-export const devServer = {
-    static: './dist',
-};
-export const entry = {
-    "main": resolve(__dirname, 'src/index.js'),
-};
-export const output = {
-    path: resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true,
-};
-export const plugins = [
-    new HtmlWebpackPlugin({
-        title: 'Food App',
-        template: "./src/template.html",
-        filename: "index.html",
-        inject: "body",
-    }),
-    new HtmlWebpackPlugin({
-        template: "./src/menu.html",
-        filename: "menu.html",
-        inject: "body",
-    }),
-    new HtmlWebpackPlugin({
-        template: 'src/contact.html', 
-        filename: 'contact.html',
-        inject: "body",
-    })
-];
-export const module = {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ],
-        },
-    ]
-};
-export const optimization = {
-    runtimeChunk: 'single',
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    entry: {
+        "main": path.resolve(__dirname, './src/script/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Food App',
+            template: path.resolve(__dirname, './src/pages/template.html'),
+            filename: "index.html",
+            inject: "body",
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/pages/menu.html'),
+            filename: "menu.html",
+            inject: "body",
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/pages/contact.html'),
+            filename: 'contact.html',
+            inject: "body",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+        ]
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    
 };
