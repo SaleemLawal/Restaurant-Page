0ae46f0571e492d14c676630086696651267e64d
SaleemLawal <slawal1@umbc.edu>
2024-01-12 13:17:27 -0500

    mode: production

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: 'production',
     devServer: {
         static: './dist/pages',
     },
